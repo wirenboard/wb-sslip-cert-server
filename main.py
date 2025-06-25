8b5baa34957d4cf33a8bf40e7a0dbb6f60650104
lostpoint <lostpoint@icloud.com>
2025-06-25 15:27:51 +0500

    remove unused import

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ from certbot._internal.display import obj as display_obj
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.x509.oid import NameOID
 from fastapi import FastAPI, File, Header, HTTPException
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from sqlmodel import Field, Session, SQLModel, create_engine, select
